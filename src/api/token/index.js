edeee92cf27fe4e91351ad22d88df4248350ef59
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-08-18 10:58:50 +0900

    Update index.js

--- src/api/token/index.js
+++ src/api/token/index.js
@@ -3,6 +3,6 @@ import * as tokenCtrl from './token.ctrl';
 
 const token = express.Router();
 
-token.post('/', tokenCtrl.tokenRefresh);
+token.post('/refresh', tokenCtrl.tokenRefresh);
 
 export default token;
